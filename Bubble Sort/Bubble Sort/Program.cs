1fa0fe578b7be7287a5259d141ae62b1010973f8
connorLock05 <117924198+connorLock05@users.noreply.github.com>
2023-05-10 17:15:38 +0100

    Add files via upload

---
+++ Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static bool Sorted(List<double> nums)
+        {
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    continue;
+                } else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Sort(ref List<double> nums)
+        {
+            do
+            {
+                for (int i = 0; i < nums.Count - 1; i++)
+                {
+                    if (nums[i] > nums[i + 1])
+                    {
+                        double temp = nums[i + 1];
+                        nums[i + 1] = nums[i];
+                        nums[i] = temp;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            } while (!(Sorted(nums)));
+        }
+
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            string inp = "";
+            while (inp != "sort")
+            {
+                Console.Write("Input: ");
+                inp = Console.ReadLine();
+                if (inp != "sort")
+                {
+                    numbers.Add(Convert.ToDouble(inp));
+                }
+            }
+
+            Sort(ref numbers);
+            foreach (double num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
